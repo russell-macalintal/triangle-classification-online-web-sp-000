cd7fdc00d6e0e87ae6dc1ea0ca29f111bad28696
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-01 02:10:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,11 @@
 class Triangle
   # write code here
+  attr_accessor :s1, :s2, :s3
+  
   def initialize(side_1, side_2, side_3)
-
+    @s1 = side_1
+    @s2 = side_2
+    @s3 = side_3
   end
 
   def kind
